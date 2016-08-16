924d3c3399155cf8ecd54c98acc6bc3e29e04fbf
Pooja Voladoddi <voladodd@usc.edu>
2016-08-15 19:46:51 -0700

    Added a few comments.

--- ch1_Matrices/gfg_SearchRowColSortedMatrix.cc
+++ ch1_Matrices/gfg_SearchRowColSortedMatrix.cc
@@ -28,11 +28,27 @@ int searchRow(const vector< vector<int> > &a, int c, int elem)
 	return -1;
 }
 
-int main() {
-	// your code goes here
+int main(){
+    
+    // Initialize a matrix of NxN.
+    // This line below compiles only with GCC flag '-std=c++11' or '-std=c++14'
 	vector< vector <int> > matrix({{1,7,15,21},{2,8,16,24},{3,10,18,26}});
+    
+    // read element to be searched.
 	int elem;
     cin >> elem;
+    
+    /*
+     The algorithm works in O(N) if matrix is NxN, O(M+N) if matrix is MxN.
+     1. Sweep through columns to narrow down search space to a single column.
+        Return "c" if elem found in "c", else return "c-1".
+     
+     2. Search all rows with "c" fixed (returned in step 1.) 
+     
+     3. Print "True" if found, else print "False".
+     
+     [Note] Can be modified to print out location if "True".
+     */
     
 	int col = searchCol(matrix,elem);
 	int rows = searchRow(matrix, col, elem);
@@ -41,5 +57,7 @@ int main() {
 	    cout << "False\n";
 	else
 	    cout << "True\n";
+    
+    
 	return 0;
 }
